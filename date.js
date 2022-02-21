f8408fcad56f2d6edc8170447092dc4ac31744e2
11carmel11 <11carmrl11@gmail.com>
2022-02-21 20:06:58 +0200

    YY MM DD date format validator

--- date.js
+++ date.js
@@ -4,3 +4,9 @@ const isYYYY_DD_MM = (date) => {
   return regex.test(date);
 };
 console.log(isYYYY_DD_MM("1999-10-03"));
+
+const isYY_DD_MM = (date) => {
+  const regex = /^(\d{2})-(0[1-9]|1[012])-([12][0-9]|3[01]|0[1-9])$/;
+  return regex.test(date);
+};
+console.log(isYY_DD_MM("99-10-03"));
