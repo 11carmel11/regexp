e0c3db752bd430bea87cbc165739bb603e0ada80
11carmel11 <11carmrl11@gmail.com>
2022-02-22 10:23:47 +0200

    added case detector function

--- string.js
+++ string.js
@@ -25,3 +25,17 @@ const isSnakeCase = (str) => {
 };
 
 console.log(isSnakeCase("snake_case"));
+
+const whatAmI = (str) => {
+  if (!str) return null;
+
+  const casesFunctions = [isCamelCase, isKebabCase, isPascalCase, isSnakeCase];
+
+  for (const func of casesFunctions) {
+    if (func(str)) {
+      const caseName = func.name.match(/[A-Z][a-z]{4,}/)[0]; // just for showing off!
+      return `The string \`${str}\` is ${caseName} case!`;
+    }
+  }
+};
+console.log(whatAmI("k_k"));
